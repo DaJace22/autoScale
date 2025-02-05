c0a953402ee76b9fc3b2104ef388793ddd81bed7
DaJace22 <lokeynakrn@yahoo.com>
2025-02-05 15:18:58 -0500

    Update vpc.tf
    Changed AZs to us-east2's

--- vpc.tf
+++ vpc.tf
@@ -13,7 +13,7 @@ resource "aws_vpc" "custom_vpc" {
 variable "vpc_availability_zone" {
   type        = list(string)
   description = "Availability zone"
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  default     = ["us-east-2a", "us-east-2b", "us-east-2c"]
 }
 
 resource "aws_subnet" "public_subnet" {
